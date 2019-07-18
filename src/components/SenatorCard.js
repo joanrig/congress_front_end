9bf9451226d0d2162f7f12c9d7449fdda06497eb
github username <github email address>
2019-07-18 19:26:12 -0400

    added twitter and youtube to senator cards

--- src/components/SenatorCard.js
+++ src/components/SenatorCard.js
@@ -25,8 +25,18 @@ class SenatorCard extends React.Component {
   render() {
      // let URL = `${BASE_PATH}\`${this.props.fieldName}\``
      const FB_PATH = "https://www.facebook.com/"
-     let account = this.props.senator.facebook_account
-     let fb_page = FB_PATH + account
+     let fb_account = this.props.senator.facebook_account
+     let fb_page = FB_PATH + fb_account
+
+     const TWITTER_PATH = "https://twitter.com/"
+     let twitter_account = this.props.senator.twitter_account
+     let twitter_page = TWITTER_PATH + twitter_account
+
+     const YOUTUBE_PATH = "https://www.youtube.com/user/"
+     let youtube_account = this.props.senator.you_tube_account
+     let youtube_page = TWITTER_PATH + youtube_account
+
+     
 
     return (
       <Card>
@@ -37,7 +47,7 @@ class SenatorCard extends React.Component {
         <span className='next-election'>Up for re-election: {this.props.senator.next_election} </span>
       </Card.Meta>
       <Card.Description>
-        <a href={fb_page}>Facebook</a>
+        <a href={fb_page}>Facebook</a>   <a href={twitter_page}>Twitter</a>   <a href={youtube_page}>YouTube</a>
       </Card.Description>
     </Card.Content>
     <Card.Content extra>

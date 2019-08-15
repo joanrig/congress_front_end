6236918d29f47b4f0cc60424c5d0e36e02fc63f2
github username <github email address>
2019-08-15 12:10:09 -0400

    refactored MemberSorter

--- src/components/Member/MemberSorter.js
+++ src/components/Member/MemberSorter.js
@@ -11,33 +11,33 @@ class MemberSorter extends React.Component {
     super()
   }
 
-
   render (){
     let tips = `You can sort after searching or vice-versa!`
 
-    let chamber
-    let sortingButtons
-    if (this.props.renderedBy === "senate"){
-      chamber = "Senate"
-      sortingButtons =
-        <>
-          <Button size="large" color="green" onClick={() => this.props.fetchSenateLoyalists()}>Party Loyalists</Button>
-          <Button size="large" color="blue" onClick={() => this.props.fetchSenateMavericks()}>Party Mavericks</Button>
-          <Button size="large" color="red" onClick={() => this.props.fetchSenateTruants()}>Most Truant</Button>
-          <Button size="large" color="black" onClick={() => this.props.fetchSenateBySeniority()}>Longest Serving</Button>
-          <Button size="large" color="orange" onClick={() => this.props.fetchSenateByAge()}>Oldest</Button>
-        </>
-    } else {
-      chamber = "House of Representatives"
-      sortingButtons =
-        <>
-          <Button size="large" color="green" onClick={() => this.props.fetchHouseLoyalists()}>Party Loyalists</Button>
-          <Button size="large" color="blue" onClick={() => this.props.fetchHouseMavericks()}>Party Mavericks</Button>
-          <Button size="large" color="red" onClick={() => this.props.fetchHouseTruants()}>Most Truant</Button>
-          <Button size="large" color="black" onClick={() => this.props.fetchHouseBySeniority()}>Longest Serving</Button>
-          <Button size="large" color="orange" onClick={() => this.props.fetchHouseByAge()}>Oldest</Button>
-        </>
+    let loyalists
+    let mavericks
+    let truants
+    let seniority
+    let age
 
+    let actions = this.props
+    let chamber
+
+    if (actions.renderedBy === "senate"){
+      chamber = "Senate"
+      loyalists = actions.fetchSenateLoyalists
+      mavericks = actions.fetchSenateMavericks
+      truants = actions.fetchSenateTruants
+      seniority = actions.fetchSenateBySeniority
+      age = actions.fetchSenateByAge
+
+    }  else {
+      chamber = "House of Representatives"
+      loyalists = actions.fetchHouseLoyalists
+      mavericks = actions.fetchHouseMavericks
+      truants = actions.fetchHouseTruants
+      seniority = actions.fetchHouseBySeniority
+      age = actions.fetchHouseByAge
     }
 
 
@@ -49,7 +49,11 @@ class MemberSorter extends React.Component {
         <h4 className="center">How to play: Search or sort to narrow results, then see if you can guess the names!  Stumped? Click on "Guess Who?"</h4>
 
         <div className="center">
-          {sortingButtons}
+          <Button size="large" color="green" onClick={() => loyalists()}>Party Loyalists</Button>
+          <Button size="large" color="blue" onClick={() => mavericks()}>Party Mavericks</Button>
+          <Button size="large" color="red" onClick={() => truants()}>Most Truant</Button>
+          <Button size="large" color="black" onClick={() => seniority()}>Longest Serving</Button>
+          <Button size="large" color="orange" onClick={() => age()}>Oldest</Button>
           <Button
             size="large"
             color="yellow"

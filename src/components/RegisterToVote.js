22b2e526cfb37fb0b3b63a86154ac12a6642ce6c
github username <github email address>
2019-08-15 15:49:51 -0400

    moved register to vote styling into css

--- src/components/RegisterToVote.js
+++ src/components/RegisterToVote.js
@@ -9,7 +9,10 @@ const RegisterToVote = () => {
       <br/>
       <br/>
       <br/>
-      <iframe title="register-to-vote" src="https://register.rockthevote.com/?partner=1&source=ovrpage" width="100%" height="1200" marginheight="0" frameborder="0"></iframe>
+      <iframe
+        className = "register-to-vote"
+        title="register-to-vote"
+        src="https://register.rockthevote.com/?partner=1&source=ovrpage" ></iframe>
     </>
     )
   }

eb6c8445a624aa77165750b76723ad74b654483d
github username <github email address>
2019-08-14 21:18:33 -0400

    sized nav and sort buttons down

--- src/components/Senate/SenateSorter.js
+++ src/components/Senate/SenateSorter.js
@@ -21,14 +21,14 @@ class SenateSorter extends React.Component {
         </h1>
         <h4 className="center">How to play: Search or sort to narrow results, then see if you can guess the names!  Stumped? Click on "Guess Who?"</h4>
 
-        <div className='social center'>
-          <Button size="big" color="green" onClick={() => this.props.fetchLoyalists()}>Party Loyalists</Button>
-          <Button size="big" color="blue" onClick={() => this.props.fetchMavericks()}>Party Mavericks</Button>
-          <Button size="big" color="red" onClick={() => this.props.fetchTruants()}>Most Truant</Button>
-          <Button size="big" color="black" onClick={() => this.props.fetchBySeniority()}>Longest Serving</Button>
-          <Button size="big" color="orange" onClick={() => this.props.fetchByAge()}>Oldest</Button>
+        <div className='center'>
+          <Button size="large" color="green" onClick={() => this.props.fetchLoyalists()}>Party Loyalists</Button>
+          <Button size="large" color="blue" onClick={() => this.props.fetchMavericks()}>Party Mavericks</Button>
+          <Button size="large" color="red" onClick={() => this.props.fetchTruants()}>Most Truant</Button>
+          <Button size="large" color="black" onClick={() => this.props.fetchBySeniority()}>Longest Serving</Button>
+          <Button size="large" color="orange" onClick={() => this.props.fetchByAge()}>Oldest</Button>
           <Button
-            size="big"
+            size="large"
             color="yellow"
             className="ui icon button"
             data-tooltip={tips}>

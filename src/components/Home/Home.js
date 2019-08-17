7f694fac20a666186dbd6c224cf5981bb3d461da
github username <github email address>
2019-08-17 15:46:01 -0400

    styled intro text on home page to work for mobile and desktop

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,63 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Container } from 'semantic-ui-react'
 import HomePageGraphics from './HomePageGraphics'
 
 
-const Home = () => {
+class Home extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      width: window.innerWidth
+     }
+   }
 
-  return (
-    <Container className="center">
-      <h1>
-        Who's Who in the 116th United States Congress?
-      </h1>
-      <h4>powered by data from ProPublica and Center For Responsive Politics</h4>
-      <br/>
-      <h2 className="ui block header home intro">
-        Who's served the longest? Who's the oldest? <br/>
-        Most truant? Most Maverick? How many are women? <br/>
-        Click the buttons above to find out!
-      </h2>
-      <br/>
-      <HomePageGraphics image={'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vote-lead-01-1520883451.jpg?crop=1.00xw:1.00xh;0,0&resize=640:*'}/>
-    </Container>
-  )
+  componentWillMount() {
+     window.addEventListener('resize', this.handleWindowSizeChange);
+   }
+
+  componentWillUnmount() {
+     window.removeEventListener('resize', this.handleWindowSizeChange);
+   }
+
+  handleWindowSizeChange = () => {
+     this.setState({ width: window.innerWidth });
+   }
+
+  render(){
+
+    let isMobile
+    const width = this.state.width
+    width <= 500 ? isMobile = true : isMobile = false
+
+
+    // intro font size
+    let style
+    if (isMobile) {
+      style = "ui block header home intro mobile"
+    } else {
+      style = "ui block header home intro desktop"
+    }
+
+
+    return (
+      <Container className="center">
+        <h1>
+          Who's Who in the 116th United States Congress?
+        </h1>
+        <h4>powered by data from ProPublica and Center For Responsive Politics</h4>
+        <br/>
+        <div className={style}>
+          Who's served the longest? Who's the oldest? <br/>
+          Most truant? Most Maverick? How many are women? <br/>
+          Click the buttons above to find out!
+        </div>
+        <br/>
+        <HomePageGraphics image={'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vote-lead-01-1520883451.jpg?crop=1.00xw:1.00xh;0,0&resize=640:*'}/>
+      </Container>
+    )
+
+  }
+
+
 }
 export default Home

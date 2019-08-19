11d225215cd5aadd4e56289a94012c2ecf3fb891
github username <github email address>
2019-08-19 10:15:17 -0400

    removed get donors button, function not working

--- src/Member/MemberCard.js
+++ src/Member/MemberCard.js
@@ -181,12 +181,7 @@ class MemberCard extends Component {
             className="bills button"
             data-tooltip={legalTip}
           />
-          <Button
-            circular icon={donorsButtonIcon}
-            onClick={this.handleDonorsClick}
-            className="donors button"
-            data-tooltip={moneyTip}
-          />
+
         </div>
         <br/>
       </>
@@ -225,3 +220,11 @@ class MemberCard extends Component {
 const mapStateToProps = state => ({showDonors: state.showDonors, showBills: state.showBills})
 
 export default connect(mapStateToProps, { fetchBillsBySenator, fetchBillsByRep, getSenatorFinances, getRepFinances })(MemberCard)
+
+
+// //  <Button
+//     circular icon={donorsButtonIcon}
+//     onClick={this.handleDonorsClick}
+//     className="donors button"
+//     data-tooltip={moneyTip}
+//   />
